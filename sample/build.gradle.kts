ffaeb4b40f5cb60b91fc5cb69fa14d93d302b8b7
gorani <jiniopening@gmail.com>
2026-07-14 16:46:27 +0900

    Fix CI and shimmer interaction behavior

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.github.swiftman.shimmerui.sample"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.swiftman.shimmerui.sample"
         minSdk = 23
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
